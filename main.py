1c1bb9ed3caa9c4d4e21fbf6a8770f8d845f4faf
vakanoule1 <vdiaby1@gmail.com>
2024-11-15 20:39:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,66 +14,61 @@ print(capitalized_name)
 print("Hello " + name + ", please prepare your heart rate data.")
 
 # Take in hour one heart rate
-hour1_hrt = input("Enter your hour 1 heart rate: ")
+hour1_hrt = int(input("Enter your hour 1 heart rate: "))
 
 # Append this integer value to the list
-heart_rate.append(37)
+heart_rate.append(hour1_hrt)
 print(heart_rate)
 
 # Take in hour two heart rate
-hour2_hrt = input("Enter your hour 2 heart rate: ")
+hour2_hrt = int(input("Enter your hour 2 heart rate: "))
 
 # Append this integer value to the list
-heart_rate.append(40)
+heart_rate.append(hour2_hrt)
 print(heart_rate)
 
 # Take in hour three heart rate
-hour3_hrt = input("Enter your hour 3 heart rate: ")
-
+hour3_hrt = int(input("Enter your hour 3 heart rate: "))
 
 # Append this integer value to the list
-heart_rate.append(45)
+
+heart_rate.append(hour3_hrt)
 print(heart_rate)
 
 # Take in hour four heart rate
-hour4_hrt = input("Enter your hour 4 heart rate: ")
-
+hour4_hrt = int(input("Enter your hour 4 heart rate: "))
 
 # Append this integer value to the list
-heart_rate.append(32)
+heart_rate.append(hour4_hrt)
 print(heart_rate)
 
 # Take in hour five heart rate
-hour5_hrt = input("Enter your hour 5 heart rate: ")
-
+hour5_hrt = int(input("Enter your hour 5 heart rate: "))
 
 # Append this integer value to the listheart_rate.append(42)
-heart_rate.append(41)
+heart_rate.append(hour5_hrt)
 print(heart_rate)
 
 # Take in hour six heart rate
-hour6_hrt = input("Enter your hour 6 heart rate: ")
-
+hour6_hrt = int(input("Enter your hour 6 heart rate: "))
 
 # Append this integer value to the list
-heart_rate.append(36)
+heart_rate.append(hour6_hrt)
 print(heart_rate)
 
 # Take in hour seven heart rate
-hour7_hrt = input("Enter your hour 7 heart rate: ")
-
+hour7_hrt = int(input("Enter your hour 7 heart rate: "))
 
 # Append this integer value to the list
-heart_rate.append(33)
+heart_rate.append(hour7_hrt)
 print(heart_rate)
 
 # Take in hour eight heart rate
-hour8_hrt = input("Enter your hour 8 heart rate: ")
-
+hour8_hrt = int(input("Enter your hour 8 heart rate: "))
 
 # Append this integer value to the list
-#heart_rate.append(50)
-
+heart_rate.append(hour8_hrt)
+print(heart_rate)
 
 # Thank them: "Thank you 'name', we will now calculate your health metrics."
 print("Thank you Diaby, we will now calculate your health metrics.")
@@ -96,16 +91,20 @@ diff = maximum - minimum
 print(diff)
 
 # if this difference is greater than 30, print that they experienced waking during sleep
-print("they experienced waking during sleep, if", diff > 30)
+if diff > 30
+ print("they experienced waking during sleep")
 # if this difference is greatert han 20, print that their HRV is in a healthy range
-print("their HRV is in a healthy range,if", diff > 20)
+elif diff > 20
+  print("their HRV is in a healthy range")
 # otherwise, print out that their HRV is low and might indicate sleep issues
-print("their HRV is low and might indicate sleep issues")
+else
+  print("their HRV is low and might indicate sleep issues")
 
 # print out their entire heart rate data
 Print("The user's maximum heart rate during sleep is :", maximum)
-print(The user's minimum heart rate during sleep is: ", minimum)
-print(The difference between the maximum and minimum heart rates is: ", diff)
-print(A message indicating the user's potential sleep quality based on the HRV analysis.)
-print("The entire list of heart rate data entered by the user is: " heart_rate)
+print("The user's minimum heart rate during sleep is: ", minimum)
+print("The difference between the maximum and minimum heart rates is: ", diff)
+print("A message indicating the user's potential sleep quality based on the HRV analysis.)
+print("The entire list of heart rate data entered by the user is: ", heart_rate)
+
 
